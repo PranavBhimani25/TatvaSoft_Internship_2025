a660ace7a15442cc2276e07977aad877e740cc52
PranavBhimani25 <pranavbhimani04@gmail.com>
2025-06-03 00:24:10 +0530

    Add project files.

---
+++ Day4/Book_Management2/Book.Services/Services/BooksService.cs
@@ -0,0 +1,59 @@
+﻿using Book.DataAccess;
+using Book.DataAccess.Models;
+using Book.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Services.Services
+{
+    public class BooksService
+    {
+        private readonly AppDbContext _context;
+
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Mybooks>> GetAllAsync() => await _context.MyBooks.ToListAsync();
+
+        public async Task<Mybooks?> GetByIdAsync(int id) => await _context.MyBooks.FindAsync(id);
+
+        public async Task<Mybooks> AddAsync(Mybooks book)
+        {
+            _context.MyBooks.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Mybooks?> UpdateAsync(int id, Mybooks updatedBook)
+        {
+            var book = await _context.MyBooks.FindAsync(id);
+            if (book == null) return null;
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Genre = updatedBook.Genre;
+
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var book = await _context.MyBooks.FindAsync(id);
+            if (book == null) return false;
+
+            _context.MyBooks.Remove(book);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Mybooks>> FilterByGenreAsync(string genre) =>
+            await _context.MyBooks.Where(b => b.Genre == genre).ToListAsync();
+    }
+}
---
+++ Day4/Book_Management2/Book_Management2/Controller/BookController.cs
@@ -0,0 +1,52 @@
+﻿using Book.DataAccess.Models;
+using Book.Services.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Management2.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksService _service;
+
+        public BooksController(BooksService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+            Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var book = await _service.GetByIdAsync(id);
+            return book is null ? NotFound() : Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Mybooks book) =>
+            Ok(await _service.AddAsync(book));
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Mybooks book)
+        {
+            var updated = await _service.UpdateAsync(id, book);
+            return updated is null ? NotFound() : Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _service.DeleteAsync(id);
+            return deleted ? NoContent() : NotFound();
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> FilterByGenre([FromQuery] string genre) =>
+            Ok(await _service.FilterByGenreAsync(genre));
+    }
+}
